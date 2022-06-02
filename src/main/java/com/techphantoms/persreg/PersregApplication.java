196f794b91d8795b82039bc6a759953def625dda
Sachin Kumar <sachin8447@gmail.com>
2022-06-02 23:17:31 +0530

    Intial Commit

---
+++ src/main/java/com/techphantoms/persreg/PersregApplication.java
@@ -0,0 +1,13 @@
+package com.techphantoms.persreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersregApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersregApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techphantoms/persreg/PersregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techphantoms.persreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersregApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
