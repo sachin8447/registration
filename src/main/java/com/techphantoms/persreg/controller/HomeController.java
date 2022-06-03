b33416cc2c8420b6292bc81ff3dc6be57917b28c
Sachin Kumar <sachin8447@gmail.com>
2022-06-03 10:16:56 +0530

    Intial Commit

---
+++ src/main/java/com/techphantoms/persreg/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.techphantoms.persreg.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+   @GetMapping("/healthCheck")
+    public String toString() {
+        return "WorkingFine";
+    }
+}
---
+++ src/main/java/com/techphantoms/persreg/controller/RegistrationController.java
@@ -0,0 +1,9 @@
+package com.techphantoms.persreg.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+
+
+}
